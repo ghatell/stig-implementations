1c54c750b3b05a135302d63888289086a7277121
Soroush Asadi <soroushasad@gmail.com>
2025-04-02 23:14:09 +0100

    Create WN10-CC-000035.ps1

---
+++ WN10-CC-000035.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+    Prevents the release of the NetBIOS name on demand by setting NoNameReleaseOnDemand to 1.
+
+.NOTES
+    Author          : Soroush Asadi
+    LinkedIn        : https://www.linkedin.com/in/soroush-asadi-881098178/
+    GitHub          : https://github.com/ghatell
+    Date Created    : 2025-04-02
+    Last Modified   : 2025-04-02
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000035
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 5.1
+
+.DESCRIPTION
+    Applies the STIG setting to disable NetBIOS name release on demand, reducing the attack surface for NetBIOS spoofing.
+
+.USAGE
+    PS C:\Users\cyberSentinel_92\Desktop\WN10-CC-000035.ps1
+#>
+
+# Define the registry path
+$regPath = "HKLM:\SYSTEM\CurrentControlSet\Services\Netbt\Parameters"
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    Write-Output "Registry path not found. Creating: $regPath"
+    New-Item -Path $regPath -Force | Out-Null
+} else {
+    Write-Output "Registry path already exists: $regPath"
+}
+
+# Set NoNameReleaseOnDemand to 1
+Set-ItemProperty -Path $regPath -Name "NoNameReleaseOnDemand" -Value 1 -Type DWord
+
+# Confirm the setting
+$currentValue = Get-ItemProperty -Path $regPath -Name "NoNameReleaseOnDemand"
+Write-Output "NoNameReleaseOnDemand successfully set to: $($currentValue.NoNameReleaseOnDemand)"
