6d1aafc26b72e9bef55eeee32e2228131b96e087
Soroush Asadi <soroushasad@gmail.com>
2025-04-02 17:19:56 +0100

    Create WN10-CC-000275.ps1

---
+++ WN10-CC-000275.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    Disables drive redirection in Remote Desktop sessions by setting fDisableCdm to 1.
+
+.NOTES
+    Author          : Soroush Asadi
+    GitHub          : https://github.com/ghatell
+    LinkedIn        : https://www.linkedin.com/in/soroush-asadi-881098178/
+    Date Created    : 2025-03-31
+    Version         : 1.0
+    STIG ID         : WN10-CC-000275
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 5.1
+
+.DESCRIPTION
+    Enforces a policy to prevent local drive access within Remote Desktop sessions for added security.
+
+.USAGE
+    PS C:\Users\cyberSentinel_92\Desktop\WN10-CC-000275.ps1
+#>
+
+# Define registry path
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+
+# Create registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    Write-Output "Registry path not found. Creating: $regPath"
+    New-Item -Path $regPath -Force | Out-Null
+} else {
+    Write-Output "Registry path already exists: $regPath"
+}
+
+# Set fDisableCdm to 1
+Set-ItemProperty -Path $regPath -Name "fDisableCdm" -Value 1 -Type DWord
+
+# Confirm the setting
+$currentValue = Get-ItemProperty -Path $regPath -Name "fDisableCdm"
+Write-Output "fDisableCdm successfully set to: $($currentValue.fDisableCdm)"
