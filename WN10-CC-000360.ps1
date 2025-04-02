9f1664628e2e86b9400d94888892191831873d1b
Soroush Asadi <soroushasad@gmail.com>
2025-04-02 15:04:03 +0100

    Create WN10-CC-000360.ps1

---
+++ WN10-CC-000360.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    Disables Digest authentication for the WinRM client by setting AllowDigest to 0.
+
+.NOTES
+    Author          : Soroush Asadi
+    GitHub          : https://github.com/ghatell
+    LinkedIn        : https://www.linkedin.com/in/soroush-asadi-881098178/
+    STIG ID         : WN10-CC-000360
+    Date Created    : 2025-03-31
+    Version         : 1.0
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 5.1
+
+.DESCRIPTION
+    Enforces the STIG requirement by disabling Digest authentication, which is less secure and can expose credentials under certain conditions.
+
+.USAGE
+    PS C:\Users\cyberSentinel_92\Desktop\WN10-CC-000360.ps1
+#>
+
+# Define registry path
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WinRM\Client"
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    Write-Output "Registry path not found. Creating: $regPath"
+    New-Item -Path $regPath -Force | Out-Null
+} else {
+    Write-Output "Registry path already exists: $regPath"
+}
+
+# Set AllowDigest to 0
+Set-ItemProperty -Path $regPath -Name "AllowDigest" -Value 0 -Type DWord
+
+# Confirm the setting
+$currentValue = Get-ItemProperty -Path $regPath -Name "AllowDigest"
+Write-Output "AllowDigest successfully set to: $($currentValue.AllowDigest)"
